ac3c2f34379fb2fb3daae0f056b59f25952f21a7
Raul <you@example.com>
2022-05-24 09:04:12 +0200

    new change

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React en Factoría F5
         </a>
       </header>
     </div>
